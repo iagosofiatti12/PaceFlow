514b3a410ffbb84d9675f5a51e69ac4ce826d579
Iago <iagoribeiro12@hotmail.com>
2026-07-15 19:41:16 -0300

    ci: adicionar workflow de lint, tipos, formatacao e testes
    - GitHub Actions roda ESLint, tsc --noEmit, prettier --check e Jest
      em todo push na main e em todo pull request
    - prettier com endOfLine auto para nao brigar com o CRLF do Windows
    - normaliza formatacao dos configs antigos (.eslintrc, babel, jest)

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  preset: "react-native",
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    "node_modules/(?!(react-native|@react-native|@react-navigation|react-navigation|expo|@expo)/)",
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-navigation|expo|@expo)/)',
   ],
-  testMatch: ["**/__tests__/**/*.test.(js|ts|tsx)"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  testMatch: ['**/__tests__/**/*.test.(js|ts|tsx)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   collectCoverageFrom: [
-    "src/**/*.{js,jsx,ts,tsx}",
-    "!src/**/*.test.{js,jsx,ts,tsx}",
-    "!src/**/__tests__/**",
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/**/__tests__/**',
   ],
 };
